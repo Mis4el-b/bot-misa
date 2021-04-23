0765a4f84546d4d012528c8f64939dc0a4201613
Mr-Pato <80291498+Mr-Pato@users.noreply.github.com>
2021-04-23 08:59:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ const {
   + 'VERSION:3.0\n'
   + 'FN:Mr.Pato\n' // Nama
   + 'ORG:Mr.Pato-Bot;\n' // Nama bot
-  + 'TEL;type=CELL;type=VOICE;waid=17693039228:+1 769 303-9223n' // Nomor bot
+  + 'TEL;type=CELL;type=VOICE;waid=447520607252:+44 7520 607252n' // Nomor bot
   + 'END:VCARD' 
   
   
